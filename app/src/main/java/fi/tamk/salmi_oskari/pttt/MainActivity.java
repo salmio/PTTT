d85b31276be5cc10df8aba68f5345cf63a8230f6
Oskari <salmari123@gmail.com>
2017-04-18 14:24:44 +0300

    Initial commit

---
+++ app/src/main/java/fi/tamk/salmi_oskari/pttt/MainActivity.java
@@ -0,0 +1,23 @@
+package fi.tamk.salmi_oskari.pttt;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
